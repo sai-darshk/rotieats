b8e10a15e76328f2bb9eb7c1941314d9bffa9779
sai-darshk <saidarshk@gmail.com>
2023-03-21 11:22:53 -0500

    removed netflify

--- app.js
+++ app.js
@@ -28,19 +28,3 @@ elements.forEach(element => {
   }, 500);
 });
 
-/** Here's how to say Hello to Netlify Graph! */
-await fetch('https://graph.netlify.com/graphql?app_id=b83d3c66-7e5b-47e6-96b4-5ef3cc6f57b6&show_metrics=false',
-  {
-    method: 'POST',
-    body: JSON.stringify({
-      query: `query HelloNetlifyGraph {
-    helloGraph(sessionId: "794ce8d8-8491-4337-8d12-51bacbaab391") {
-      welcome
-      about
-      whatToDoNext
-    }
-  }`,
-    }),
-  }
-)
-.then((r) => r.json())
